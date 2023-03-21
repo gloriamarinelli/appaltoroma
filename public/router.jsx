0bf96271e668afeaa37f8c285b0b8be7cf61f5b8
Gloria Marinelli <gloria.marinelli22@gmail.com>
2023-03-21 09:56:32 +0100

    fix sitemap

---
+++ public/router.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route } from 'react-router';
+ 
+export default (
+    <Route>
+        <Route path='/' />
+        <Route path='/about' />
+        <Route path='/projects' />
+        <Route path='/bonus' />      
+    </Route>
+);
